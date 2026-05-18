cab95cfac5c93450edbac0512457dcb5752e599e
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-05-18 21:06:32 +0530

    Db using Sqlite3

---
+++ app/exp/create_db.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect('site.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+               CREATE TABLE user(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               name TEXT NOT NULL,
+               email TEXT UNIQUE NOT NULL)
+               ''')
+
+conn.commit()
+conn.close()
