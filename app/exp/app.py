d1876d95692c4531174e4f512e423bccfc4a4223
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-05-19 08:42:20 +0530

    db using SQLAlchemy

---
+++ app/exp/app.py
@@ -0,0 +1,6 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+#db.init_(app)
+
+#app = Flask()
---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(200), unique=True)
+    post = db.relationship('Post', backref='author', lazy=True)
+
+db.create_all()
+User.query.all()
