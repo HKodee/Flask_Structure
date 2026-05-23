990f59081d959ce0efddca2fbb31b0db9db289af
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-05-23 22:21:14 +0530

    app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-bd = SQLAlchemy()
+db = SQLAlchemy()
 
 def crate_app():
     app = Flask(__name__)
 
     app.config['SECERET_KEY'] = "your-secret-key"
+    app.config['SQL_DATABAS_URI'] = "sqllite:///shop.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+    db.__init__(app)
+
+    from app.routes.auth import auth_bp
+    from app.routes.auth import task_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(task_bp)
+
 
     return app
