0fafbb9697c3d0b1977edebede6224359cf2c5ce
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-05-26 14:01:39 +0530

    user class and register

---
+++ app/routes/auth.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Hash password
+        hashed_password = generate_password_hash(password)
+
+        # Create user
+        new_user = User(
+            username=username,
+            email=email,
+            password=hashed_password
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/login')
+
+    return render_template('register.html')
+
+# @auth_bp.route('/login', methods=["GET", "POST"])
+# def login():
+#     if request.method == "POST":
+#         username = request.form.get('username')
+        
+        
